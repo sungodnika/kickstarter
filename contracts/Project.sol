6fa40279c246a0c9079b2c65901a05ecce751cf3
Prolok <prolok.sundaresan@play.co>
2021-08-13 09:09:39 +0530

    removed safemath as its redundant now

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,15 +2,12 @@
 pragma solidity ^0.8.6;
 
 // import "@openzeppelin/contracts/access/Ownable.sol"; TODO to make use of this.
-import '@openzeppelin/contracts/utils/math/SafeMath.sol';
-
 
 contract Project {
-  using SafeMath for uint;
   // Storage
-  uint public projectDuration = 30 days;
-  uint public contributionLowerBound = 0.01 ether;
-  uint public goalMinimum = 1 ether;
+  uint constant public projectDuration = 30 days;
+  uint constant public contributionLowerBound = 0.01 ether;
+  uint constant public goalMinimum = 1 ether;
 
   address public owner;
   uint public goal;
@@ -30,10 +27,10 @@ contract Project {
   //sets the owner to sender
   constructor(address _owner, uint _goal) {
     // set deadline and update owner and goal
+    require(msg.sender != address(0), "Not valid address");
     owner = _owner;
     goal = _goal;
     deadline = block.timestamp + projectDuration;
-    availableFunds = 0;
     state = ProjectState.ongoing;
 
   }
@@ -69,20 +66,20 @@ contract Project {
     evaluateState();
     require(state == ProjectState.ongoing, 'Project has ended, cannot contribute');
     require(msg.sender != address(0), "Not valid address");
-    require(msg.value >= 0.01 ether, "Minimum contribution amount 0.01 ETH");
-    contributions[msg.sender] = contributions[msg.sender].add(msg.value);
-    availableFunds = availableFunds.add(msg.value);
+    require(msg.value >= contributionLowerBound, "Minimum contribution amount 0.01 ETH");
+    contributions[msg.sender] += msg.value;
+    availableFunds += msg.value;
     emit Contributed(msg.sender, msg.value, contributions[msg.sender], availableFunds);
   }
 
   
-  function cancelProject() external onlyOwner returns(bool){
+  function cancelProject() external onlyOwner {
     // change state to failed
     evaluateState();
     require(state == ProjectState.ongoing, 'Project has ended, cannot cancel');
     state = ProjectState.failed;
     emit CanceledProject(address(this), owner);
-    return true;
+    // return true; does return cost gas??
   }
 
   // validate endTime has passed and goal has been reached
@@ -90,9 +87,9 @@ contract Project {
     // check if the amount is available and let owner withdraw.
     evaluateState();
     require(state == ProjectState.successful, 'Project is not successful');
-    require(percentAmount>=0 && percentAmount <=100);
-    uint amount = availableFunds.mul(percentAmount).div(100);
-    availableFunds =  availableFunds.sub(amount);
+    require(percentAmount <=100);
+    uint amount = availableFunds * percentAmount / 100;
+    availableFunds -= amount;
     (bool success, ) = msg.sender.call{value:amount}("");
     if(success) {
       emit Withdraw(msg.sender, amount);
@@ -104,9 +101,9 @@ contract Project {
     // let the caller withdraw the full amount
     evaluateState();
     require(state == ProjectState.failed, 'Project has not failed');
-    require(contributions[msg.sender]>0, 'Contributer has no funds');
+    require(contributions[msg.sender] > contributionLowerBound, 'Contributer has no funds');
     uint amount = contributions[msg.sender];
-    availableFunds = availableFunds.sub(amount);
+    availableFunds -=amount;
     contributions[msg.sender]=0;
     (bool success, ) = msg.sender.call{value:amount}("");
     if(success) {
